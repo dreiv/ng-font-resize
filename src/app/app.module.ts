97dd7d8588a6f46247bcfdb140f9697c0d258710
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-01-10 10:56:59 +0200

    figured I'd do it with a directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FitTextDirective } from './fit-text/fit-text.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FitTextDirective
   ],
   imports: [
     BrowserModule,
