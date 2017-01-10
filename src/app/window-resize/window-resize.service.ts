19b68194b5f9faf88ed39551ff00cd36c8a4f781
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-01-10 11:41:46 +0200

    added a delay to the window size pooling of the observable.

--- src/app/window-resize/window-resize.service.ts
+++ src/app/window-resize/window-resize.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 
+const CHECK_DELAY = 250;
+
 @Injectable()
 export class WindowResizeService {
   width$: Observable<number>;
@@ -10,6 +12,7 @@ export class WindowResizeService {
     this.width$ = (windowSize$.pluck('width') as Observable<number>).distinctUntilChanged();
 
     Observable.fromEvent(window, 'resize')
+      .throttleTime(CHECK_DELAY)
       .map(getWindowWidth)
       .subscribe(windowSize$);
   }
