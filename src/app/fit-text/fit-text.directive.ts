01600ffcd56981e7403f4b78b38cd44278f511e2
dreiv <Solidchest1>
2017-01-11 20:42:59 +0200

    added some logic, does resize but not correctly,

--- src/app/fit-text/fit-text.directive.ts
+++ src/app/fit-text/fit-text.directive.ts
@@ -6,12 +6,56 @@ import { WindowService } from '../window/window.service';
   providers: [WindowService]
 })
 export class FitTextDirective {
+  domParent: any;
+  domElement: any;
+  domElemStyle: any;
+
+  computed : Function = (element) => {window.getComputedStyle(element, null)};
+  newLines : Function = () => {this.domElement.children.length || 1};
+  lineHeight : Function = () => {this.computed('line-height')};
+  display : Function = () => {this.computed('display')};
+
+  min : string = '1';
+  max : string = '250';
+  compressor : number = 1;
+  calcSize : number = 10;
 
   constructor(private el: ElementRef,
               private windowService: WindowService) {
+    this.domElement = this.el.nativeElement;
+    this.domElemStyle = this.domElement.style;
+    this.domParent = this.domElement.parentElement;
+
     windowService.width$.subscribe((width: number) => {
-      console.log('window resized to ', width);
+      console.log('applying resizer, window resized to ', width);
+      this.resizer();
     });
+  }
+
+  private resizer() {
+    // Don't calculate for elements with no width or height
+    if (this.domElement.offsetHeight * this.domElement.offsetWidth === 0)
+      return;
+
+    // Set standard values for calculation
+    this.domElemStyle.fontSize       = this.calcSize + 'px';
+    this.domElemStyle.lineHeight     = '1';
+    this.domElemStyle.display        = 'inline-block';
+
+    // Set usage values
+    this.domElemStyle.fontSize       = this.calculate() + 'px';
+    this.domElemStyle.lineHeight     = this.lineHeight;
+    this.domElemStyle.display        = this.display;
+  }
+
+  private calculate() {
+    const ratio = (this.calcSize * this.newLines()) / this.domElement.offsetWidth / this.newLines();
+    return Math.max(
+      Math.min((this.domParent.offsetWidth - 6) * ratio * this.compressor,
+        parseFloat(this.max)
+      ),
+      parseFloat(this.min)
+    )
   }
 
 }
